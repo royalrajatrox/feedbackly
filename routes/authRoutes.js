9d806f7f3c900bc9458755fd9d6f1cbfca72f4b0
rajatdahiya <royalrajatrox@gmail.com>
2024-11-28 21:04:57 -0400

    [ADD] production keys

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,6 +8,21 @@ module.exports = (app) => {
         })
     );
 // There could be many scopes, for ex. contact info... this means getting the information of the client from the google
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get('/auth/google/callback', passport.authenticate('google', {
+        failureRedirect: '/auth/google',
+        successRedirect: '/api/current_user'
+    }));
+
+
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
+
+
+    app.get('/api/current_user', (req, res) => {
+        //req = incoming request, res = outgoing response
+        res.send(req.user);
+    });
 
 };
--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,22 @@ const passport = require("passport");
 const {Strategy: GoogleStrategy} = require("passport-google-oauth20");
 const mongoose = require('mongoose');
 const keys = require("../config/keys");
+
+
 const User = mongoose.model('users');
+
+passport.serializeUser( (user, done) => {
+    done(null, user.id);
+    // done 1 argument is error and identifying piece of information
+    // user.id 2 argument is the unique id of the record in the database inside collection
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).
+        then(user => {
+            done(null, user);
+    })
+});
 
 passport.use(
     new GoogleStrategy(
@@ -10,11 +25,13 @@ passport.use(
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
+            // using the keys of google API
     },
         (accessToken, refreshToken, profile, done) => {
             // done is called when authentication is done
         User.findOne({googleId: profile.id})
             .then( (exisitingUser) => {
+                // this exisitingUser is come from the database
                 if (exisitingUser){
                     // we already have a record of the user
                     done(null, exisitingUser);

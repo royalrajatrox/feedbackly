41c703697f5066fe5f5033bf66344988058b2459
RajatMCAF <rajat.dahiya@mcaf.nb.ca>
2024-12-04 15:11:27 -0400

    [ADD] Survey.js

--- models/Survey.js
+++ models/Survey.js
@@ -10,8 +10,10 @@ const surveySchema = new Schema({
    subject: String,
    recipients: [RecipientSchema] ,   //using sub document collection
    yes: { type: Number, default: 0},
-   no: { type: Number, default: 0}
-
+   no: { type: Number, default: 0},
+   _user: { type: Schema.Types.ObjectId, ref: 'User' },
+   dateSent: Date,
+   lastResponded: Date
 
 });
 
